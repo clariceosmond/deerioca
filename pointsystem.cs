a7f1f0d88a137b78b31daefc567ff6106b410fb2
DenissaTing <47044109+DenissaTing@users.noreply.github.com>
2019-01-27 03:26:39 -0500

    Add files via upload

--- pointsystem.cs
+++ pointsystem.cs
@@ -4,23 +4,44 @@ using UnityEngine;
 
 public class pointsystem : MonoBehaviour
 {
-    public static int leekpoints;
-    public static int flourpoints;
-    public static int cornpoints; 
-    
+    public int leekpoints;
+    public int flourpoints;
+    public int cornpoints;
+    public int bombpoints;
+    public bool leveluppoints;
+    public bool levelfailpoints;
+
+    public levelupsound soundd;
+    public Levelfailsound sounddd;
+
     // Start is called before the first frame update
     void Start()
     {
+
         leekpoints = 0;
-        flourpoints = 2;
+        flourpoints = 0;
         cornpoints = 0;
+        bombpoints = 0;
+        leveluppoints = false;
+        levelfailpoints = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((leekpoints == 2) && (flourpoints == 2) && (cornpoints == 4)){
+        if (bombpoints == 3)
+        {
+            levelfailpoints = true;
+            Debug.Log("Failure");
+            sounddd.PlayMusic();
+
+
+        }
+        else if ((leekpoints >= 2) && (flourpoints >= 2) && (cornpoints >= 4) && (bombpoints < 3))
+        {
+            leveluppoints = true;
             Debug.Log("Finish");
+            soundd.PlayMusic();
         }
     }
 }
